e614be05baab46ddb72bf20e996cab45fc345ebd
PralhadTech <ptalmale9623@gmail.com>
2024-08-13 16:43:32 +0530

    Api issue resolved

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { MENU_LIST_URL } from "../constant";
+import { IMG_CDN_URL, MENU_LIST_URL } from "../constant";
 
 const RestaurantMenu = () => {
   const [menu, setMenu] = useState([]);
+  const [menuList, setMenuList] = useState([]);
   const { resId } = useParams();
 
   useEffect(() => {
@@ -11,30 +12,41 @@ const RestaurantMenu = () => {
   }, []);
 
   async function getMenuList() {
-    const response = await fetch(MENU_LIST_URL);
+    const response = await fetch(MENU_LIST_URL + resId);
     const jsonData = await response.json();
-    console.log(jsonData);
-    const list = jsonData.data.cards[2].card.card.info;
-    const recomendList =
-      jsonData.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card
-        .itemCards;
-    setMenu(list, recomendList);
-    // console.log(list);
+    console.log(jsonData.data);
+    const detail = jsonData.data.cards[2].card.card.info;
+    const list = jsonData.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards;
+    console.log(list);
+    setMenu(detail);
+    setMenuList(list);
   }
   return (
-    <div className="">
-      <h1 className=" font-extrabold">{menu.name}</h1>
-      <h1>{menu.avgRating}</h1>
-      <h1>({menu.totalRatingsString})</h1>
-      <h1>{menu.costForTwoMessage}</h1>
-      <h1>{menu.cuisines}</h1>
+    <>
+      <div className="border-2  justify-center flex flex-col p-2 mt-2">
+        <h1 className=" font-bold">
+          {menu.name} {menu.avgRating} {menu.totalRatingsString}{" "}
+          {menu.costForTwoMessage}
+        </h1>
+        <h1>
+          {menu.city} {menu.areaName}
+        </h1>
+        <h1>{menu.cuisines}</h1>
+        {/* <span>
+          {menu.sla.lastMileTravelString} {menu.sla.minDeliveryTime} -{" "}
+          {menu.sla.maxDeliveryTime} mins
+        </span> */}
+      </div>
 
-      <h1>{menu.areaName}</h1>
-
-      <h5>Menu</h5>
-
-      <ul></ul>
-    </div>
+      <div className="text-center font-serif">
+        MENU:
+        {/* <ul>
+          {menuList.map((item) => {
+            <li>wel</li>;
+          })}
+        </ul> */}
+      </div>
+    </>
   );
 };
 
--- src/constant.js
+++ src/constant.js
@@ -5,4 +5,4 @@ export const IMG_CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_LIST_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=20740&catalog_qa=undefined&submitAction=ENTER";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=";
