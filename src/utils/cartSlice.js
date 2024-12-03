dafd411831988e8e67306dc0f1e2eac3d96a70f8
PralhadTech <ptalmale9623@gmail.com>
2024-12-03 11:44:32 +0530

    cart component changes

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -21,6 +21,18 @@ const cartSlice = createSlice({
       // no need of sction as making array empty
       state.items = [];
     },
+    increaseQuantity: (state, action) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item) {
+        item.quantity++;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item && item.quantity > 1) {
+        item.quantity--;
+      }
+    },
   },
 });
 //also need to export actions
